0d761bdf3e317cf6b14049e08839b4d0b51ab452
sandeepk27 <nanisandeep876@gmail.com>
2023-11-27 01:23:58 +0530

    day_3

---
+++ day_3.py
@@ -0,0 +1,32 @@
+a = complex(8, 2)
+b = True
+c = "sandy"
+d = None
+e=1.4
+print(a)
+print(b)
+a1 = 9
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+print("The type of d is ", type(d))
+print("The type of e is ", type(e))
+print("The type of a1 is ", type(a1))
+
+
+#list
+list = [8, 2.3, [-4, 5], ["apple", "banana","mango"]]
+print(list)
+
+# tuple
+t= (("parrot", "jack","sparrow"), ("Lion", "Tiger","elephant"))
+print(t)
+
+# dictionary
+dic1= {"name":"Sakshi", "age":22, "canVote":True}
+dic2= {"name":"Sandeep", "age":24, "canVote":True}
+dic3= {"name":"Akanksha", "age":17, "canVote":False}
+print(dic1)
+print(dic3)
+print(dic2)
