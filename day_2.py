8752a8b5c8e0a1e7df5c2ef006c58ba5b2596478
sandeepk27 <nanisandeep876@gmail.com>
2023-11-26 20:44:31 +0530

    day 2

---
+++ day_2.py
@@ -0,0 +1,20 @@
+import pandas  # This is an example of external module
+import hashlib  # This is an example of built in module
+
+print("Hi")
+
+# Dont worry about how to use these modules just yet!
+# pandas.read_csv("one.csv")
+# m = hashlib.sha256()
+
+print(5+3)
+print("Sandeep")
+print(4-1)
+b=345
+d=123
+c=564
+a=342
+print(a-d+c-b)
+print(a*d-b/c)
+print(a//b)
+print(a/b)
