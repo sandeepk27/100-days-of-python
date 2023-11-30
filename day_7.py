81505ffc9c3128a1eacb7bce7747fe47853fd48b
sandeepk27 <nanisandeep876@gmail.com>
2023-12-01 00:27:38 +0530

    day_7

---
+++ day_7.py
@@ -0,0 +1,9 @@
+# Taking Input from user
+
+a = input("Enter your name: ")
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+print("Your name is", a)
+print(x  + y)
+
+print(int(x) + int(y))
