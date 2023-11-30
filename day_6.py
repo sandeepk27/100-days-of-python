f444b542d0d2362cedc27bc91e3cb496e5274f53
sandeepk27 <nanisandeep876@gmail.com>
2023-11-30 19:05:02 +0530

    updated

--- day_6.py
+++ day_6.py
@@ -1,12 +1,15 @@
-
+# Type casting in python
 a = "1"
 # a = 1
 b = "2"
 # b = 2
+print(a+b)
 print(int(a) + int(b))
 
 # Implicit TypeCasting
 c = 1.9
-d = 8
+d = 8.23
 
 print(c + d) 
+print(int(c))
+print(int(d))
