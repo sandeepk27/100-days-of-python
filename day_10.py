e186aaa25cabb06bf8bf48e10c71326a37a554aa
sandeepk27 <nanisandeep876@gmail.com>
2023-12-04 01:00:51 +0530

    update

--- day_10.py
+++ day_10.py
@@ -9,6 +9,7 @@ print(a.replace("Sandy", "Shiv"))
 print(a.split(" "))
 blogHeading = "strings in python"
 print(blogHeading.capitalize())
+print(blogHeading.lower())
 
 str1 = "Welcome to the Console!!!"
 print(len(str1))
@@ -23,34 +24,35 @@ print(str1.endswith("to", 4, 10))
 
 str1 = "He's name is Dan. He is an honest man."
 print(str1.find("ishh"))
-# print(str1.index("ishh"))
+print(str1.index("s"))
+print(str1.count('H'))
 
-str1 = "WelcomeToTheConsole"
-print(str1.isalnum())
-str1 = "Welcome"
-print(str1.isalpha())
+# str1 = "WelcomeToTheConsole"
+# print(str1.isalnum())
+# str1 = "Welcome"
+# print(str1.isalpha())
 
-str1 = "hello world"
-print(str1.islower())
+# str1 = "hello world"
+# print(str1.islower())
 
-str1 = "We wish you a Merry Christmas\n"
-print(str1.isprintable())
-str1 = "         "       #using Spacebar
-print(str1.isspace())
-str2 = "  "       #using Tab
-print(str2.isspace())
+# str1 = "We wish you a Merry Christmas\n"
+# print(str1.isprintable())
+# str1 = "         "       #using Spacebar
+# print(str1.isspace())
+# str2 = "  "       #using Tab
+# print(str2.isspace())
 
-str1 = "World Health Organization" 
-print(str1.istitle())
+# str1 = "World Health Organization" 
+# print(str1.istitle())
 
-str2 = "To kill a Mocking bird"
-print(str2.istitle())
+# str2 = "To kill a Mocking bird"
+# print(str2.istitle())
 
-str1 = "Python is a Interpreted Language" 
-print(str1.startswith("Python"))
+# str1 = "Python is a Interpreted Language" 
+# print(str1.startswith("Python"))
 
-str1 = "Python is a Interpreted Language" 
-print(str1.swapcase())
+# str1 = "Python is a Interpreted Language" 
+# print(str1.swapcase())
 
-str1 = "His name is Dan. Dan is an honest man."
-print(str1.title())
+# str1 = "His name is Dan. Dan is an honest man."
+# print(str1.title())
