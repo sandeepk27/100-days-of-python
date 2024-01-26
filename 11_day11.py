496f3276623f1d2f582494ca5996999f112aa0be
sandeepk27 <nanisandeep876@gmail.com>
2024-01-26 16:01:10 +0530

    updated

--- 11_day11.py
+++ 11_day11.py
@@ -15,3 +15,11 @@ else:
   print("You cannot drive")
   print("No")
   print("Yay!")
+  
+  
+applePrice = 10
+budget = 200
+if (budget - applePrice > 50):
+    print("Alexa, add 1 kg Apples to the cart.")
+else:
+    print("Alexa, do not add Apples to the cart.")
