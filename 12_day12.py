a342f08cc1346d72b75d7b3478a46707fac1f148
sandeepk27 <nanisandeep876@gmail.com>
2024-01-26 16:03:54 +0530

    day_12

---
+++ 12_day12.py
@@ -0,0 +1,15 @@
+# conditional statement
+
+# if elif condtion
+
+num = int(input("Enter the value of num: "))
+if (num < 0):
+  print("Number is negative.")
+elif (num == 0):
+  print("Number is Zero.")
+elif (num == 999):
+  print("Number is Special.")
+else:
+  print("Number is positive.")
+
+print("Completed today's task Successfully..")
