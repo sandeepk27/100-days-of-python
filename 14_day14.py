c79a50adab0e08fa9c54e2039cb8df0c9349f765
sandeepk27 <nanisandeep876@gmail.com>
2024-01-27 17:50:38 +0530

    updated

--- 14_day14.py
+++ 14_day14.py
@@ -11,3 +11,30 @@ timestamp = time.strftime('%S')
 print(timestamp)
 
 # https://docs.python.org/3/library/time.html#time.strftime
+
+from datetime import datetime
+
+dt_object = datetime(2022, 1, 27, 12, 0, 0)  # Replace with your datetime
+timestamp = dt_object.timestamp()
+
+print("Datetime:", dt_object)
+print("Timestamp:", timestamp)
+
+
+# sleep using timestamp
+import time
+
+seconds_to_sleep = 5
+print(f"Sleeping for {seconds_to_sleep} seconds...")
+time.sleep(seconds_to_sleep)
+print("Awake!")
+
+
+# working with timezones
+
+from datetime import datetime
+import pytz
+
+dt_object = datetime.now(pytz.timezone('US/Eastern'))
+print("Current Time in US/Eastern:", dt_object)
+
