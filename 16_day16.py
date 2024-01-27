38bcbfc3a27555a5ab962621056092844be5b569
sandeepk27 <nanisandeep876@gmail.com>
2024-01-28 01:04:39 +0530

    new

--- 16_day16.py
+++ 16_day16.py
@@ -1,2 +1,15 @@
-Day 16 of code!
-Write your code here.
+# Day 16 of code!
+# Write your code here.
+
+# name = 'Sandeep'
+# for i in name:
+#   print(i)
+#   if(i =="e"):
+#     print("This is something special!")
+    
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for color in colors:
+#   print(color)
+#   for i in color:
+#     print(i)
+
