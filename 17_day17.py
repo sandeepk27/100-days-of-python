ca2bf187a335c83341b49436916cdd1f1713cc09
sandeepk27 <nanisandeep876@gmail.com>
2024-01-29 00:10:40 +0530

    new

--- 17_day17.py
+++ 17_day17.py
@@ -1,2 +1,13 @@
-Day 17 of code!
-Write your code here.
+# Day 17 of code!
+# Write your code here.
+
+# While loops
+
+# i = int(input("Enter the number: "))
+# print(i)
+# while(i<=38):
+#   i = int(input("Enter the number: "))
+#   print(i)
+
+# print("Done with the loop")
+
