8fb333bb619763ffe3aee360fc0f65e431baced2
sandeepk27 <nanisandeep876@gmail.com>
2024-01-30 21:38:50 +0530

    updated

--- 19_day19.py
+++ 19_day19.py
@@ -13,8 +13,8 @@ def isGreater(a, b):
   else:
     print("Second number is greater or equal")
 
-def isLesser(a, b):
-  pass
+# def isLesser(a, b):
+#   pass
   
 
 a = 9
@@ -23,9 +23,9 @@ isGreater(a, b)
 calculateGmean(a, b)
 # gmean1 = (a*b)/(a+b)
 # print(gmean1)
-c = 8
-d = 74
-isGreater(c, d)
-calculateGmean(c, d)
-# gmean2 = (c*d)/(c+d)
-# print(gmean2)
+# c = 8
+# d = 74
+# isGreater(c, d)
+# calculateGmean(c, d)
+# # gmean2 = (c*d)/(c+d)
+# # print(gmean2)
