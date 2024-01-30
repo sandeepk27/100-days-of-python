b15ff6c04c64ee8f6c83e562bdd2c4192335b639
sandeepk27 <nanisandeep876@gmail.com>
2024-01-30 21:32:43 +0530

    updated

--- 18_day18.py
+++ 18_day18.py
@@ -1,2 +1,17 @@
-Day 18 of code!
-Write your code here.
+# Day 18 of code!
+# Write your code here.
+
+# brake and continue
+
+# for i in range(13):
+#   if(i == 10):
+#     print("Skip the iteration")
+#     continue
+#   print("5 X", i, "=", 5 * i)
+  
+i = 0
+while True:
+  print(i)
+  i = i + 1
+  if(i%100 == 0):
+    break
