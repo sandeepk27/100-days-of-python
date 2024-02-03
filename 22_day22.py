a6a679a2078cadac206c2d1b6db5f786e5cbaaab
sandeepk27 <nanisandeep876@gmail.com>
2024-02-03 22:23:17 +0530

    updated

--- 22_day22.py
+++ 22_day22.py
@@ -1,2 +1,22 @@
-Day 22 of code!
-Write your code here.
+# Day 22 of code!
+# Write your code here.
+
+# list methods
+
+l = [11, 45, 1, 2, 4, 6, 1, 1]
+print(l)
+print(l[2])
+print(l[5])
+# l.append(7)
+# l.sort(reverse=True)
+# l.reverse()
+# print(l.index(1))
+# print(l.count(1))
+# m = l.copy()
+# m[0] = 0
+# l.insert(1, 899)
+m = [900, 1000, 1100]
+k = l + m
+# print(k)
+# l.extend(m)
+print(l)
--- 23_day23.py
+++ 23_day23.py
@@ -1,2 +1,14 @@
-Day 23 of code!
-Write your code here.
+# Day 23 of code!
+# Write your code here.
+
+# tuples
+
+t = (1, 2, 76, 342, 32, "green", True)
+# t[0] = 90
+print(type(t), t)
+# print(t[34])
+
+if  3421 in t:
+  print("Yes 342 is present in this tle")
+t2 = t[1:4]
+print(t2)
