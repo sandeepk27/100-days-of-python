8c7e6ac897224ead005fb11fecbd6ac62dd11b95
sandeepk27 <nanisandeep876@gmail.com>
2024-02-04 00:07:07 +0530

    new

--- 24_day24.py
+++ 24_day24.py
@@ -1,2 +1,11 @@
-Day 24 of code!
-Write your code here.
+# Day 24 of code!
+# Write your code here.
+
+# Operations on Tuples
+
+tuple1 = (0, 1, 2, 3, 2, 31, 1, 3, 2, 3)
+# res = tuple1.count(3)
+
+res = tuple1.index(3, 4, 8)
+# res = len(tuple1)
+print('Count of 3 in tuple1 is:', res)
