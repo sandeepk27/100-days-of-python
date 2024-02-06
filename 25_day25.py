9c72baee022faf2e9fc5bfb80d4bf03959945fd0
sandeepk27 <nanisandeep876@gmail.com>
2024-02-06 22:48:14 +0530

    new

--- 25_day25.py
+++ 25_day25.py
@@ -1,2 +1,2 @@
-Day 25 of code!
-Write your code here.
+# Day 25 of code!
+# Write your code here.
--- 26_day26.py
+++ 26_day26.py
@@ -1,2 +1,2 @@
-Day 26 of code!
-Write your code here.
+# Day 26 of code!
+# Write your code here.
