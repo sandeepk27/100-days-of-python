c28a0ff115c277ce2c7f0a25de40c4b2355750a0
sandeepk27 <nanisandeep876@gmail.com>
2024-02-07 00:15:39 +0530

    update

--- 25_day25.py
+++ 25_day25.py
@@ -1,8 +1,18 @@
 # Day 25 of code!
-
 # Write your code here.
 
+# Excercise 1
 
+import time
+t = time.strftime('%H:%M:%S') 
+hour = int(time.strftime('%H'))
+# hour = int(input("Enter hour: "))
+# print(hour)
 
-print('day 25 coding')
+if(hour>=0 and hour<12):
+  print("Good Morning Sir!")
+elif(hour>=12 and hour<17):
+  print("Good Afternoon Sir!")
+elif(hour>=17 and hour<0):
+  print("Good Night Sir!")
 
