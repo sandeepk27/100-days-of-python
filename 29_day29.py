ff10d98f8d953ab5f3beb7dc93cb9352844b4390
sandeepk27 <nanisandeep876@gmail.com>
2024-02-08 21:33:50 +0530

    new

--- 29_day29.py
+++ 29_day29.py
@@ -1,2 +1,16 @@
-Day 29 of code!
-Write your code here.
+# Day 29 of code!
+# Write your code here.
+
+# Sets
+
+# s = {2, 4, 2, 6}
+# print(s)
+
+# info = {"Carla", 19, False, 5.9, 19}
+# print(info)
+
+# # harry = set()
+# # print(type(harry))
+
+# for value in info:
+#   print(value)
