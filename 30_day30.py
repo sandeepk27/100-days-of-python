1d0b912dddd56545df502c522fc23fd979161ae7
sandeepk27 <nanisandeep876@gmail.com>
2024-02-10 01:58:30 +0530

    update

--- 30_day30.py
+++ 30_day30.py
@@ -3,8 +3,8 @@
 
 # Set methods
 
-# info = {"Carla", 19, False, 5.9}
-# if "Carla" in info:
-#     print("Carla is present.")
-# else:
-#     print("Carla is absent.")
+info = {"Carla", 19, False, 5.9}
+if "Carla" in info:
+    print("Carla is present.")
+else:
+    print("Carla is absent.")
