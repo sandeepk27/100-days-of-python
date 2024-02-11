7119c52c50de38f1cb44b4be8578332e11f76341
sandeepk27 <nanisandeep876@gmail.com>
2024-02-12 00:06:17 +0530

    updated

--- 32_day32.py
+++ 32_day32.py
@@ -3,12 +3,12 @@
 
 # dictionary methods
 
-# d1 = {122: 45, 123: 89, 567: 69, 670: 69}
-# d2 = {222: 67, 566: 90}
+d1 = {122: 45, 123: 89, 567: 69, 670: 69}
+d2 = {222: 67, 566: 90}
 
-# # d1.update(d2)
-# # d1.clear()
-# # d1.pop(122)
+# d1.update(d2)
+# d1.clear()
+# d1.pop(122)
 # d1.popitem()
 # del d1[122]
-# print(d1) 
+print(d1) 
--- 33_day33.py
+++ 33_day33.py
@@ -3,15 +3,15 @@
 
 # for loop with else
 
-# i = 0
-# while i<7:
-#   print(i)
-#   i = i + 1
-#   # if i == 4:
-#   #   break
+i = 0
+while i<7:
+  print(i)
+  i = i + 1
+  # if i == 4:
+  #   break
 
-# else:
-#   print("Sorry no i")
+else:
+  print("Sorry no i")
 
 # for x in range(5):
 #     print ("iteration no {} in for loop".format(x+1))
