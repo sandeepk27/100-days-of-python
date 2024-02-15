c0752739e9ef2c7a883d474f86134ca594bedd16
sandeepk27 <nanisandeep876@gmail.com>
2024-02-15 09:41:36 +0530

    new

--- 41_day41.py
+++ 41_day41.py
@@ -1,2 +1,25 @@
-Day 41 of code!
-Write your code here.
+# Day 41 of code!
+# Write your code here.
+
+# from math import sqrt, pi
+# from math import pi, sqrt as s
+# import math as math_builtin_python
+
+# result = math_builtin_python.sqrt(9) * math_builtin_python.pi
+# print(result)  # Output: 3.0
+
+
+def welcome():
+  print("Hey you are welcome my friend")
+
+
+Sid = "A good boy"
+
+# from harry import welcome, harry
+# import Sid as S
+# import math
+
+# print(dir(math))
+# print(math.nan, type(math.nan))
+# S.welcome()
+# print(S.harry)
