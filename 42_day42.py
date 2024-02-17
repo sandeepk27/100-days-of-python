a80509233af36108210967693686b58b0a89fba2
sandeepk27 <nanisandeep876@gmail.com>
2024-02-17 19:22:00 +0530

    new

--- 42_day42.py
+++ 42_day42.py
@@ -1,2 +1,18 @@
-Day 42 of code!
-Write your code here.
+# Day 42 of code!
+# Write your code here.
+# importing
+
+import sid
+
+sid.welcome()
+
+
+# main
+
+def welcome():
+  print("Hey you are welcome from Sandeep")
+
+# print(__name__)
+
+if __name__ == "__main__":
+  welcome()
