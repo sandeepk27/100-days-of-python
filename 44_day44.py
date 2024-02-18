03b8ad148958fa43e48db3754daa5f9fb77d2f76
sandeepk27 <nanisandeep876@gmail.com>
2024-02-19 00:18:37 +0530

    new

--- 44_day44.py
+++ 44_day44.py
@@ -1,2 +1,13 @@
-Day 44 of code!
-Write your code here.
+# Day 44 of code!
+# Write your code here.x = 10  # global variable
+
+# local vs global variables
+def my_function():
+  global x
+  x = 5  # this will change the value of the global variable x
+  y = 5  # local variable
+
+
+my_function()
+print(x)  # prints 5
+# print(y) # this will cause an error because y is a local variable and is not accessible outside of the function
