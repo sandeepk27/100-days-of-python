ed67d0f103fb0afecca116dcd8118fec2db7e042
sandeepk27 <nanisandeep876@gmail.com>
2024-02-18 11:36:42 +0530

    new

--- 43_day43.py
+++ 43_day43.py
@@ -1,2 +1,31 @@
-Day 43 of code!
-Write your code here.
+# Day 43 of code!
+# Write your code here.
+
+# import os
+
+# if(not os.path.exists("data")):
+#     os.mkdir("data")
+
+# for i in range(0, 100):
+#     os.mkdir(f"data/Day{i+1}")
+    
+##
+# import os 
+# folders = os.listdir("data")
+
+# print(os.getcwd())
+# os.chdir("/Users")
+# print(os.getcwd())
+
+# for folder in folders:
+#     print(folder)
+#     print(os.listdir(f"data/{folder}"))
+    
+
+# ##
+# import os
+ 
+
+# for i in range(0, 100):
+#     os.rename(f"data/Tutorial{i+1}", f"data/Tutorial {i+1}")
+    
