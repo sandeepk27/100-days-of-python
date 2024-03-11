196ec76c06d25948c74495d62aa6011069e0fc83
Sandeep <nanisandeep876@gmail.com>
2024-03-11 21:09:49 +0530

    updated

--- 65_day65.py
+++ 65_day65.py
@@ -1,2 +1,15 @@
-Day 65 of code!
-Write your code here.
+# Day 65 of code!
+# Write your code here.
+
+# dict & help methods
+class Person:
+  def __init__(self, name, age):
+      self.name = name
+      self.age = age
+      self.version = 1
+
+    
+p = Person("Jhonny", 30)
+print(p.__dict__)
+
+print(help(Person))
