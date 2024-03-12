133bd756dbf3ea0b5e8ea2d0c968194ab92dd033
Sandeep <nanisandeep876@gmail.com>
2024-03-13 02:17:04 +0530

    updated

--- 67_day67.py
+++ 67_day67.py
@@ -1,2 +1,12 @@
-Day 67 of code!
-Write your code here.
+# Day 67 of code!
+# Write your code here.
+
+# Magic Dunder methods
+from emp import Employee
+
+e = Employee("Harry")
+print(str(e))
+print(repr(e))
+# print(e.name)
+# print(len(e))
+e()
