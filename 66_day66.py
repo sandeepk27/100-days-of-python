3450252411bb35c7647b06feebed9170b21d25f6
Sandeep <nanisandeep876@gmail.com>
2024-03-12 21:18:08 +0530

    updated

--- 66_day66.py
+++ 66_day66.py
@@ -1,2 +1,20 @@
 # Day 66 of code!
 # Write your code here.
+
+# super keyword
+
+class Employee:
+  def __init__(self, name, id):
+    self.name = name
+    self.id = id
+
+class Programmer(Employee):
+  def __init__(self, name, id, lang):
+    super().__init__( name, id)
+    self.lang = lang
+
+rohan = Employee("Rohan Das", "420")
+sandy = Programmer("Sandy", "2345", "Python")
+print(sandy.name)
+print(sandy.id)
+print(sandy.lang)
