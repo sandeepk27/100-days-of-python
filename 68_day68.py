3ef71b14132cdb297a66008fa4c139fa36ea82ff
Sandeep <nanisandeep876@gmail.com>
2024-03-14 00:14:33 +0530

    updated

--- 68_day68.py
+++ 68_day68.py
@@ -1,2 +1,24 @@
-Day 68 of code!
-Write your code here.
+# Day 68 of code!
+# Write your code here.
+
+# method overriding
+
+class Shape:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    
+  def area(self):
+      return self.x * self.y
+
+class Circle(Shape):
+    def __init__(self, radius):
+      self.radius = radius
+      super().__init__(radius, radius)
+
+    def area(self):
+        return 3.14 *  super().area()
+      
+rec = Shape(3, 5)
+print(rec.area())
+
