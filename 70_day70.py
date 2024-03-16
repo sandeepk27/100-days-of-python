0efb470fb38513bf49f5764ea2517a3b3b4df718
Sandeep <nanisandeep876@gmail.com>
2024-03-16 21:11:44 +0530

    update

--- 70_day70.py
+++ 70_day70.py
@@ -1,2 +1,26 @@
-Day 70 of code!
-Write your code here.
+# Day 70 of code!
+# Write your code here.
+
+# Single Inheritance
+
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        
+    def make_sound(self):
+        print("Sound made by the animal")
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        Animal.__init__(self, name, species="Dog")
+        self.breed = breed
+        
+    def make_sound(self):
+        print("Bark!")
+
+d = Dog("Dog", "Doggerman")
+d.make_sound()
+
+a = Animal("Dog", "Dog")
+a.make_sound()
