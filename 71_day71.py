c22197056bdbe8ee829188672b9fe6d5c6bdd5b2
Sandeep <nanisandeep876@gmail.com>
2024-03-19 09:34:01 +0530

    updated

--- 71_day71.py
+++ 71_day71.py
@@ -1,2 +1,28 @@
 # Day 71 of code!
 # Write your code here.
+
+# Multiple Inheritance
+
+class Employee:
+  def __init__(self, name):
+    self.name = name
+  def show(self):
+    print(f"The name is {self.name}")
+
+class Dancer:
+  def __init__(self, dance):
+    self.dance = dance
+
+  def show(self):
+    print(f"The dance is {self.dance}")
+
+class DancerEmployee(Employee, Dancer):
+  def __init__(self, dance, name):
+    self.dance = dance
+    self.name = name
+
+o  = DancerEmployee("Pop", "Neha")
+print(o.name)
+print(o.dance)
+o.show() 
+print(DancerEmployee.mro())
