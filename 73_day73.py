899f88d7dbf95ac0eb87c10942dc44e853f7afb0
Sandeep <nanisandeep876@gmail.com>
2024-08-26 11:18:35 +0530

    new

--- 73_day73.py
+++ 73_day73.py
@@ -10,3 +10,16 @@ for file in files:
     print(file)
     os.rename(f"clutteredFolder/{file}", f"clutteredFolder/{i}.png")
     i = i + 1
+    
+    
+#from PyPDF2 import PdfWriter
+# import os
+
+# merger = PdfWriter()
+# files = [file for file in os.listdir() if file.endswith(".pdf")]
+
+# for pdf in files:
+#     merger.append(pdf)
+
+# merger.write("merged-pdf.pdf")
+# merger.close()
