94599c9d4b65bc5eccacc5bef5cef4a6acc5321c
Sandeep <nanisandeep876@gmail.com>
2024-08-26 11:15:30 +0530

    new

--- 74_day74.py
+++ 74_day74.py
@@ -1,2 +1,28 @@
-Day 74 of code!
-Write your code here.
+# Day 74 of code!
+# Write your code here.
+
+# Example of Hybrid Inheritance 
+class BaseClass:
+  pass
+
+class Derived1(BaseClass):
+  pass  
+
+class Derived2(BaseClass):
+  pass  
+
+class Derived3(Derived1, Derived2):
+  pass
+
+# Hierarchical Inheritance
+class BaseClass:
+  pass
+
+class D1(BaseClass):
+  pass
+
+class D2(BaseClass):
+  pass
+
+class D3(D1):
+  pass
